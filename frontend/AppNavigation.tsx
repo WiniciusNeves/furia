18f1ccf0d458bc5c91472ded6d55c4921dd426a0
WiniciusNeves <winiciusneves2004@hotmail.com>
2025-04-28 20:09:22 -0300

    feat: implementar tela de splash e navegação inicial com onboarding e autenticação

---
+++ frontend/AppNavigation.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import App from './App';
+import Onboarding from './src/screens/onboarding';
+import Onboarding2 from './src/screens/onboarding2';
+import Onboarding3 from './src/screens/onboarding3';
+import Auth from './src/screens/auth';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="App" >
+        <Stack.Screen name="App" component={App} options={{ headerShown: false }} />
+        <Stack.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding2" component={Onboarding2} options={{ headerShown: false }} />
+        <Stack.Screen name="onboarding3" component={Onboarding3} options={{ headerShown: false }} />
+        <Stack.Screen name="auth" component={Auth} options={{ headerShown: false }} />
+
+        
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
