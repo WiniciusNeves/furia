4199a336b9c740fe69cc9c31440657cd16b817ed
WiniciusNeves <winiciusneves2004@hotmail.com>
2025-05-02 22:00:06 -0300

    feat: atualizar configuração do Firebase para usar variáveis de ambiente

--- frontend/src/config/firebaseConfig.tsx
+++ frontend/src/config/firebaseConfig.tsx
@@ -6,15 +6,26 @@ import {
   Auth,
 } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
+
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCePcEH9Ya8KhDLGMclUPGrIIPhmDXlk8k",
-  authDomain: "chatbot-furia-c1fd2.firebaseapp.com",
-  projectId: "chatbot-furia-c1fd2",
-  storageBucket: "chatbot-furia-c1fd2.appspot.com",
-  messagingSenderId: "853140539901",
-  appId: "1:853140539901:web:6a41f38a58e1254c52f194",
-  measurementId: "G-B6TCBRL9N0"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
